c904009de7d4cc2cccac8dda270ab51b7a8ac96f
felipeIotti <felipeizago@hotmail.com>
2025-03-15 23:34:39 -0300

    production ajusts

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,10 +4,11 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production"]).default("production"),
   DATABASE_URL: z.string(),
+  DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
   JWT_SECRET: z.string(),
   EMAIL: z.string(),
   PASSWORD: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 });
 
 const _env = envSchema.safeParse(process.env);
