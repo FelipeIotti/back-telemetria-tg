6d8e87cf6806da32174fbbbef9e15447a83905c4
felipeIotti <felipeizago@hotmail.com>
2025-03-17 23:43:16 -0300

    production ajusts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -17,7 +17,7 @@ export async function appRoutes(app: FastifyInstance) {
   });
   app.post("/auth", auth);
 
-  app.get("/base-data", { onRequest: [verifyJWT] }, listBaseData);
+  app.get("/base-data", listBaseData);
   app.post("/base-data", { onRequest: [verifyJWT] }, createBaseData);
   app.get("/base-data/last", { onRequest: [verifyJWT] }, getLastBaseData);
 
